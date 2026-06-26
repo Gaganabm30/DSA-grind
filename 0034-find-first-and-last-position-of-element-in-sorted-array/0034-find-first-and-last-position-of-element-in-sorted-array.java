6c517cef08b8ba921cb66ff6f9bf30cba2c83bf2
Gagana B M <gagugagana01@gmail.com>
2026-06-26 08:43:38 -0500

    Time: 0 ms (100%), Space: 48.2 MB (30.62%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,80 +1,39 @@
 class Solution {
-
-    public int[] searchRange(int[] nums,
-                             int target) {
-
-        int first =
-            firstOccurrence(nums,target);
-
-        int last =
-            lastOccurrence(nums,target);
-
-        return new int[]{first,last};
-    }
-
-    private int firstOccurrence(int[] nums,
-                                int target){
-
-        int low = 0;
-        int high = nums.length - 1;
-
-        int ans = -1;
-
-        while(low <= high){
-
-            int mid =
-                low + (high-low)/2;
-
-            if(nums[mid] == target){
-
-                ans = mid;
-                high = mid - 1;
-            }
-
-            else if(nums[mid] < target){
-
-                low = mid + 1;
-            }
-
-            else{
-
-                high = mid - 1;
+    public int[] searchRange(int[] arr, int target) {
+            int[] ans = {-1,-1};
+            int first=firstOcc(arr,target);
+            int last=lastOcc(arr,target);
+            return new int[] {first, last};
+   }
+    private int firstOcc(int[] arr, int target){
+        int low=0, high=arr.length-1, ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                high=mid-1;
+            }else if(arr[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-
         return ans;
     }
 
-    private int lastOccurrence(int[] nums,
-                               int target){
-
-        int low = 0;
-        int high = nums.length - 1;
-
-        int ans = -1;
-
-        while(low <= high){
-
-            int mid =
-                low + (high-low)/2;
-
-            if(nums[mid] == target){
-
-                ans = mid;
-                low = mid + 1;
-            }
-
-            else if(nums[mid] < target){
-
-                low = mid + 1;
-            }
-
-            else{
-
-                high = mid - 1;
+    private int lastOcc(int[] arr, int target){
+         int low=0, high=arr.length-1, ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                low=mid+1;
+            }else if(arr[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-
         return ans;
     }
 }
