96d1b00de08f3ea0f6e9e6af6ee47ed73c33e406
Gagana B M <gagugagana01@gmail.com>
2026-07-01 10:19:29 -0500

    commited into recursion

--- recursion/0022-generate-parentheses/0022-generate-parentheses.java
+++ recursion/0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,21 +1,19 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-       List<String> res = new ArrayList<>();
-       addPar(0,0,"",res,n);
-       return res;
+        List<String> res = new ArrayList<>();
+        addpar(0,0,"",res,n);
+        return res;
     }
-    private void addPar(int open, int close, String s, List<String> res, int n){
+    private void addpar(int open, int close, String s, List<String> res, int n){
         if(open==close && open+close==2*n){
             res.add(s);
             return;
         }
-
         if(open<n){
-            addPar(open+1, close, s+"(", res, n);
+            addpar(open+1,close,s+"(",res,n);
         }
-        
         if(open>close){
-            addPar(open, close+1, s+")", res, n);
+            addpar(open,close+1,s+")",res,n);
         }
     }
 }
