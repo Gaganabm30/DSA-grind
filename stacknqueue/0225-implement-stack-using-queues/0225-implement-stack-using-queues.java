ece927ffedbcd8ef936808f1c68d3c8059c10964
gaganabm30 <gaganabm30@gmail.com>
2026-08-18 09:15:41 -0500

    replaced

--- stacknqueue/0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ stacknqueue/0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,32 +1,37 @@
-import java.util.*;
-
 class MyStack {
-
-    Queue<Integer> q = new LinkedList<>();
-
+    private Queue<Integer> q;
     public MyStack() {
+         q = new LinkedList<>();
     }
-
+    
     public void push(int x) {
-
-        q.offer(x);
-
-        int size = q.size();
-
-        for (int i = 0; i < size - 1; i++) {
-            q.offer(q.poll());
+        int s = q.size();
+        q.add(x);
+        for(int i=0; i<s; i++){
+            q.add(q.poll());
         }
     }
-
+    
     public int pop() {
-        return q.poll();
+        int n=q.peek();
+        q.poll();
+        return n;
     }
-
+    
     public int top() {
         return q.peek();
     }
-
+    
     public boolean empty() {
         return q.isEmpty();
     }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
